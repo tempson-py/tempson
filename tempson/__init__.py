25d69c0d389e066eb7bbfba4b1039e6c944a10ea
Jas0ncn <ceocjy@vip.qq.com>
2016-12-01 14:25:39 +0800

    update: init module

---
+++ tempson/__init__.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+
+""" Tempson Variable """
+__version__ = "0.1.0"
+__author__ = [
+    "Jas0ncn <jason@iszu.cn>"
+]
+__license__ = "MIT"
+__contributors__ = "https://github.com/tempson-py/tempson/blob/master/CONTRIBUTORS.md"
+
+from . import tempson, createFragment, createDoc, generator
+
+from .tempson import *
+from .generator import *
+from .createFragment import *
+from .createDoc import *
