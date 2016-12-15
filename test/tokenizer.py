64d558fa2f41dd45146e3ee8f91a009b602b4067
Jas0ncn <ceocjy@vip.qq.com>
2016-12-15 15:08:52 +0800

    update: update tokenize test

--- test/tokenizer.py
+++ test/tokenizer.py
@@ -1,27 +1,74 @@
 import unittest
 import tempson
 
-defaultConfig = {
-	"leftDelimiters":       '{{',
-	"rightDelimiters":      '}}',
-	"leftBlockDelimiters":  '{%',
-	"rightBlockDelimiters": '%}'
-}
+class complierTest(unittest.TestCase):
 
-class tokenizerTest(unittest.TestCase):
+    def test_empty_template(self):
+        token = tempson.complier('')
+        result = token.tokenize()
+        self.assertEqual(result, [], 'Error tokenize empty template.')
 
-    def test_Empty_template(self):
-        token = tempson.tokenizer('', defaultConfig)
-        self.assertEqual(token.tokenGen(), [''], 'Error tokenize empty template.')
+    def test_variable_template(self):
+        token = tempson.complier('<div>{{ a }}</div>')
+        result = token.tokenize()
+        self.assertEqual(result, [
+            {'type': 'HTML', 'value': '<div>'},
+            {'type': 'VAREXP', 'value': '{{ a }}'},
+            {'type': 'HTML', 'value': '</div>'}], 'Error tokenize variable template.')
 
-    def test_Variable_template(self):
-        token = tempson.tokenizer('<div>{{a}}</div>', defaultConfig)
-        self.assertEqual(token.tokenGen(), ['<div>', '{{a}}', '</div>'], 'Error tokenize variable template.')
+    def test_variable_with_expression_template(self):
+        token = tempson.complier('<div>{{ a + 1 }}</div>')
+        result = token.tokenize()
+        self.assertEqual(result, [
+            {'type': 'HTML', 'value': '<div>'},
+            {'type': 'VAREXP', 'value': '{{ a + 1 }}'},
+            {'type': 'HTML', 'value': '</div>'}], 'Error tokenize variable with expression template.')
 
-    def test_Variable_with_space_template(self):
-        token = tempson.tokenizer('<div>{{ a }}</div>', defaultConfig)
-        self.assertEqual(token.tokenGen(), ['<div>', '{{ a }}', '</div>'], 'Error tokenize variable-with-space template.')
+    def test_variable_with_string_expression_template(self):
+        token = tempson.complier('<div>{{ a + "}}" }}</div>')
+        result = token.tokenize()
+        self.assertEqual(result, [
+            {'type': 'HTML', 'value': '<div>'},
+            {'type': 'VAREXP', 'value': '{{ a + "}}" }}'},
+            {'type': 'HTML', 'value': '</div>'}], 'Error tokenize variable with string expression template.')
 
-    def test_Variable_with_keywords_template(self):
-        token = tempson.tokenizer('<div>\{{ a }}</div>', defaultConfig)
-        self.assertEqual(token.tokenGen(), ['<div>', '\{{ a }}', '</div>'], 'Error tokenize variable-with-keywords template.')
+    def test_block_template(self):
+        token = tempson.complier("""<div>
+            {% for item in list %}
+                {{ item }}
+            {% endfor %}
+        </div>""")
+        result = token.tokenize()
+        self.assertEqual(result, [
+            {'type': 'HTML', 'value': '<div>\n            '},
+            {'type': 'VAREXP', 'value': '{% for item in list %}'},
+            {'type': 'HTML', 'value': '\n                '},
+            {'type': 'VAREXP', 'value': '{{ item }}'},
+            {'type': 'HTML', 'value': '\n            '},
+            {'type': 'VAREXP', 'value': '{% endfor %}'},
+            {'type': 'HTML', 'value': '\n        </div>'}], 'Error tokenize variable with string expression template.')
+
+    def test_comment_template(self):
+        token = tempson.complier("""<div>
+            {* this is comments *}
+            {* multi row comments
+               row1
+               row2
+             *}
+        </div>""")
+        result = token.tokenize()
+        self.assertEqual(result, [
+            {'type': 'HTML', 'value': '<div>\n            '},
+            {'type': 'COMEXP', 'value': '{* this is comments *}'},
+            {'type': 'HTML', 'value': '\n            '},
+            {'type': 'COMEXP', 'value': '{* multi row comments\n               row1\n               row2\n             *}'},
+            {'type': 'HTML', 'value': '\n        </div>'}], 'Error tokenize variable with string expression template.')
+
+    def test_raw_template(self):
+        token = tempson.complier("""<div>{{{ rawHTML }}}</div>""")
+        result = token.tokenize()
+        print result
+        self.assertEqual(result, [
+            {'type': 'HTML', 'value': '<div>'},
+            {'type': 'RAWEXP', 'value': '{{{ rawHTML }}}'},
+            {'type': 'HTML', 'value': '</div>'}], 'Error tokenize variable with string expression template.')
