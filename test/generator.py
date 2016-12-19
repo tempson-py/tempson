3861136e584c43c01ca0d161ce1da1ad17b0a613
Jas0ncn <ceocjy@vip.qq.com>
2016-12-19 15:12:08 +0800

    add: add generator test

--- test/generator.py
+++ test/generator.py
@@ -1,8 +1,21 @@
+# -*- coding: utf-8 -*-
 import unittest
 import tempson
+from .coloredPrint import *
 
 class generatorTest(unittest.TestCase):
 
     def test_Empty_template(self):
-        self.template = tempson.generator("<div>{{ a }}</div>")
-        self.assertEqual(self.template.render({ "a": 123 }), '<div>{{ a }}</div>')
+        self.view = tempson.generator('<div>{{ a }}</div>')
+        result = self.view.render({ 'a': 123 })
+        try:
+            self.assertEqual(result, [
+                {'type': 'HTML', 'value': '<div>'},
+                {'type': 'VAREXP', 'value': '{{ a }}'},
+                {'type': 'HTML', 'value': '</div>'}
+            ])
+        except AssertionError:
+            coloredPrint('\n  [generator] × falied tokenize.', 'RED')
+        else:
+            coloredPrint('\n  [generator] √ successed tokenize.', 'GREEN')
+        
