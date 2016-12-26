fe3a379a46457ab3ea5fdc6fba0b0cb8a950d86b
zhounanguang <zhounanguang123@126.com>
2016-12-26 15:48:38 +0800

    fix renderer for expression undefined squence

--- tempson/renderer.py
+++ tempson/renderer.py
@@ -37,25 +37,28 @@ class renderer(object):
             template = ''
             _context = scope
             items = ast['body']
-            for i in _context[ast['squence']]:
-                _context[ast['iteratingVar']] = i
+            if _context:
+                for i in _context[ast['squence']]:
+                    _context[ast['iteratingVar']] = i
 
-                for item in items:
+                    for item in items:
 
-                    curHTML = ''
-                    valType = item['type'].upper()
-                    if (valType == 'HTML'):
-                        curHTML = item['value']
-                    elif (valType == 'FOREXP'):
-                        curHTML =  self.renderForExpression(item, _context)
-                    elif (valType == 'IFEXP'):
-                        curHTML = self.renderIfExpression(item, _context)
-                    elif (valType == 'VAREXP'):
-                        curHTML = self.renderVariable(item, _context)
-                    else:
-                        curHTML = item['value']
-                    template += curHTML
-            return template
+                        curHTML = ''
+                        valType = item['type'].upper()
+                        if (valType == 'HTML'):
+                            curHTML = item['value']
+                        elif (valType == 'FOREXP'):
+                            curHTML =  self.renderForExpression(item, _context)
+                        elif (valType == 'IFEXP'):
+                            curHTML = self.renderIfExpression(item, _context)
+                        elif (valType == 'VAREXP'):
+                            curHTML = self.renderVariable(item, _context)
+                        else:
+                            curHTML = item['value']
+                        template += curHTML
+                return template
+            else:
+                raise RuntimeError('Undefined variable for expression sequence.')
         except BaseException as e:
             raise RuntimeError(e)
     
