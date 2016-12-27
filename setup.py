cbdf1c782647bc7ad5de0d3c57bcdf0ef952d327
Jas0ncn <ceocjy@vip.qq.com>
2016-12-27 13:42:03 +0800

    add: add pypi configs

---
+++ setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+setup(
+  name = 'tempson',
+  packages = ['tempson'], # this must be the same as the name above
+  version = '1.0',
+  description = 'A template engine for Python',
+  author = 'Jas0ncn',
+  author_email = 'jason@iszu.cn',
+  url = 'https://github.com/tempson-py/tempson', # use the URL to the github repo
+  download_url = 'https://github.com/tempson-py/tempson/tarball/1.0', # I'll explain this in a second
+  keywords = ['tempson', 'template'], # arbitrary keywords
+  classifiers = [],
+)
